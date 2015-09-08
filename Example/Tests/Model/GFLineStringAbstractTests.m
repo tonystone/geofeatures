1b1715a5a738895b9d4db84d4aff4341687f50c4
Tony Stone <tony@mobilegridinc.com>
2015-09-08 12:05:05 -0700

    Add Abstract class tests - Added abstract class tests for GFPointAbstract, GFLineStringAbstract, and GFPolygonAbstract classes.

---
+++ Example/Tests/Model/GFLineStringAbstractTests.m
@@ -0,0 +1,40 @@
+/*
+*   GFLineStringAbstract.m
+*
+*   Copyright 2015 Tony Stone
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*   Created by Tony Stone on 09/08/2015.
+*/
+
+#import <GeoFeatures/GFLineStringAbstract.h>
+#import <XCTest/XCTest.h>
+
+@interface GFLineStringAbstractTests : XCTestCase
+@end
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+@implementation GFLineStringAbstractTests
+
+    - (void)testConstruction {
+
+        XCTAssertThrows([[GFLineStringAbstract alloc] init]);
+    }
+
+@end
+
+#pragma clang diagnostic pop
+
