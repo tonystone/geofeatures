3c60aafb4f31e881ecc11e5c615c340e00cc9edf
Tony Stone <hatter24@gmail.com>
2015-09-14 18:04:23 -0700

    Updated GeoFeatures.podspec version and ran "pod update" to update project files.

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -9,24 +9,24 @@
 // GeoFeatures
 #define COCOAPODS_POD_AVAILABLE_GeoFeatures
 #define COCOAPODS_VERSION_MAJOR_GeoFeatures 1
-#define COCOAPODS_VERSION_MINOR_GeoFeatures 0
-#define COCOAPODS_VERSION_PATCH_GeoFeatures 2
+#define COCOAPODS_VERSION_MINOR_GeoFeatures 1
+#define COCOAPODS_VERSION_PATCH_GeoFeatures 0
 
 // GeoFeatures/Internal
 #define COCOAPODS_POD_AVAILABLE_GeoFeatures_Internal
 #define COCOAPODS_VERSION_MAJOR_GeoFeatures_Internal 1
-#define COCOAPODS_VERSION_MINOR_GeoFeatures_Internal 0
-#define COCOAPODS_VERSION_PATCH_GeoFeatures_Internal 2
+#define COCOAPODS_VERSION_MINOR_GeoFeatures_Internal 1
+#define COCOAPODS_VERSION_PATCH_GeoFeatures_Internal 0
 
 // GeoFeatures/Internal/boost
 #define COCOAPODS_POD_AVAILABLE_GeoFeatures_Internal_boost
 #define COCOAPODS_VERSION_MAJOR_GeoFeatures_Internal_boost 1
-#define COCOAPODS_VERSION_MINOR_GeoFeatures_Internal_boost 0
-#define COCOAPODS_VERSION_PATCH_GeoFeatures_Internal_boost 2
+#define COCOAPODS_VERSION_MINOR_GeoFeatures_Internal_boost 1
+#define COCOAPODS_VERSION_PATCH_GeoFeatures_Internal_boost 0
 
 // GeoFeatures/Internal/detail
 #define COCOAPODS_POD_AVAILABLE_GeoFeatures_Internal_detail
 #define COCOAPODS_VERSION_MAJOR_GeoFeatures_Internal_detail 1
-#define COCOAPODS_VERSION_MINOR_GeoFeatures_Internal_detail 0
-#define COCOAPODS_VERSION_PATCH_GeoFeatures_Internal_detail 2
+#define COCOAPODS_VERSION_MINOR_GeoFeatures_Internal_detail 1
+#define COCOAPODS_VERSION_PATCH_GeoFeatures_Internal_detail 0
 
