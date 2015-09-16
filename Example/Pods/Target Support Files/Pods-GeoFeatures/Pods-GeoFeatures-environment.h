dc6f0177e70198a7e59745b48fd61b011c93594b
Tony Stone <hatter24@gmail.com>
2015-09-15 19:58:15 -0700

    Update podspec to release 1.2.0.

--- Example/Pods/Target Support Files/Pods-GeoFeatures/Pods-GeoFeatures-environment.h
+++ Example/Pods/Target Support Files/Pods-GeoFeatures/Pods-GeoFeatures-environment.h
@@ -9,6 +9,6 @@
 // GeoFeatures
 #define COCOAPODS_POD_AVAILABLE_GeoFeatures
 #define COCOAPODS_VERSION_MAJOR_GeoFeatures 1
-#define COCOAPODS_VERSION_MINOR_GeoFeatures 1
+#define COCOAPODS_VERSION_MINOR_GeoFeatures 2
 #define COCOAPODS_VERSION_PATCH_GeoFeatures 0
 
