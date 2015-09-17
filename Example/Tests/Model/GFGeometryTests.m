f676694dff7a77b18ea5dfc6d426f024539c12ba
Tony Stone <hatter24@gmail.com>
2015-09-17 15:13:40 -0700

    Update GFGeometryTests.m. - Added -testCopy. - Added testOverrideMethods.

--- Example/Tests/Model/GFGeometryTests.m
+++ Example/Tests/Model/GFGeometryTests.m
@@ -23,6 +23,22 @@
 @interface GFGeometryTests : XCTestCase
 @end
 
+//
+// Open up the protected methods in GFGeometry for testing.
+//
+@interface GFGeometry (Test)
+    - (instancetype) initWithWKT: (NSString *) wkt;
+@end
+
+//
+// Internal test class
+//
+@interface GFGeometryTestSubClass : GFGeometry
+@end
+@implementation GFGeometryTestSubClass
+@end
+
+
 #define GeometryWithWKTTest(wkt)              XCTAssertEqualObjects([[GFGeometry geometryWithWKT: (wkt)] toWKTString], [(wkt) uppercaseString])
 #define GeometryWithWKTTest2(wkt,expectedWKT) XCTAssertEqualObjects([[GFGeometry geometryWithWKT: (wkt)] toWKTString], [(expectedWKT) uppercaseString])
 
@@ -88,6 +104,19 @@
         EncodingTest(@"POLYGON((0 0,0 90,90 90,90 0,0 0))");
         EncodingTest(@"MULTIPOLYGON(((20 0,20 10,40 10,40 0,20 0)),((5 5,5 8,8 8,8 5,5 5)))");
         EncodingTest(@"GEOMETRYCOLLECTION(POLYGON((0 0,0 90,90 90,90 0,0 0)),POLYGON((120 0,120 90,210 90,210 0,120 0)),LINESTRING(40 50,40 140),LINESTRING(160 50,160 140),POINT(60 50),POINT(60 140),POINT(40 140))");
+    }
+
+    - (void) testCopy {
+    
+        GFGeometry * geometry = [GFGeometry geometryWithWKT: @"POINT(1 1)"];
+        
+        XCTAssertEqualObjects([[geometry copy] toWKTString], @"POINT(1 1)");
+    }
+
+    - (void) testOverriddenMethods {
+        XCTAssertThrowsSpecificNamed([[[GFGeometryTestSubClass alloc] init] toGeoJSONGeometry], NSException, NSInternalInconsistencyException);
+        XCTAssertThrowsSpecificNamed([[[GFGeometryTestSubClass alloc] init] mkMapOverlays], NSException, NSInternalInconsistencyException);
+        XCTAssertThrowsSpecificNamed([[GFGeometryTestSubClass alloc] initWithWKT: nil], NSException, NSInternalInconsistencyException);
     }
 
 @end
