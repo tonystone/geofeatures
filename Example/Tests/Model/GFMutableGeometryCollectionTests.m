901764aa0e671d99961be275bdae4e563e9c826c
Tony Stone <hatter24@gmail.com>
2015-09-28 14:13:38 -0700

    Update GFMutableGeometryCollectionTests.m - Adding varied types to the GFGeometryCollection for indexed subscripting test.

--- Example/Tests/Model/GFMutableGeometryCollectionTests.m
+++ Example/Tests/Model/GFMutableGeometryCollectionTests.m
@@ -157,10 +157,11 @@
         GFMutableGeometryCollection * geometryCollection = [[GFMutableGeometryCollection alloc] init];
 
         geometryCollection[0] = [[GFPolygon alloc] initWithWKT: @"POLYGON((20 0,20 10,40 10,40 0,20 0))"];
-        geometryCollection[1] = [[GFPolygon alloc] initWithWKT: @"POLYGON((5 5,5 8,8 8,8 5,5 5))"];
-        geometryCollection[2] = [[GFPolygon alloc] initWithWKT: @"POLYGON((4 4,4 7,7 7,7 4,4 4))"];
+        geometryCollection[1] = [[GFPoint alloc] initWithWKT: @"POINT(103 2)"];
+        geometryCollection[2] = [[GFBox alloc] initWithWKT: @"BOX(1 1,3 3)"];
+        geometryCollection[3] = [[GFLineString alloc] initWithWKT: @"LINESTRING(5 0,10 0,5 -5,5 0)"];
 
-        XCTAssertEqualObjects([geometryCollection toWKTString], @"GEOMETRYCOLLECTION(POLYGON((20 0,20 10,40 10,40 0,20 0)),POLYGON((5 5,5 8,8 8,8 5,5 5)),POLYGON((4 4,4 7,7 7,7 4,4 4)))");
+        XCTAssertEqualObjects([geometryCollection toWKTString], @"GEOMETRYCOLLECTION(POLYGON((20 0,20 10,40 10,40 0,20 0)),POINT(103 2),POLYGON((1 1,1 3,3 3,3 1,1 1)),LINESTRING(5 0,10 0,5 -5,5 0))");
     }
 
     - (void) testSetObjectAtIndexedSubscript_WithReassignValidPolygonAndValidIndex {
