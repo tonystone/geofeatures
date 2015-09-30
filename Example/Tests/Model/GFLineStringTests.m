4131126cdf93378892b075af5f3a8f81e2776f1f
Tony Stone <hatter24@gmail.com>
2015-09-30 16:36:13 -0700

    Break out GFLineStringTests.m Asserts into their own methods.

--- Example/Tests/Model/GFLineStringTests.m
+++ Example/Tests/Model/GFLineStringTests.m
@@ -39,29 +39,51 @@ static __attribute__((constructor(101),used,visibility("internal"))) void static
 
 @implementation GFLineStringTests
 
-    - (void)testConstruction {
+#pragma mark - Test init
 
+    - (void)testInit_NoThrow {
         XCTAssertNoThrow([[GFLineString alloc] init]);
+    }
+
+    - (void)testInit_NotNil {
         XCTAssertNotNil([[GFLineString alloc] init]);
     }
 
-    - (void)testFailedConstruction {
+#pragma mark - Test initWithGeoJSONGeometry
+
+    - (void) testInitWithGeoJSONGeometry_WithInvalidGeoJSON {
         XCTAssertThrowsSpecificNamed([[GFLineString alloc] initWithGeoJSONGeometry:  @{@"invalid": @{}}], NSException, NSInvalidArgumentException);
+    }
+
+#pragma mark - Test initWithWKT
+
+    - (void) testInitWithWKT_WithInvalidWKT {
         XCTAssertThrows([[GFLineString alloc] initWithWKT: @"INVALID()"]);
     }
+
+#pragma mark - Test copy
 
     - (void) testCopy {
         XCTAssertEqualObjects([[[[GFLineString alloc] initWithWKT: @"LINESTRING(100 0,101 1)"] copy] toWKTString], @"LINESTRING(100 0,101 1)");
     }
 
+#pragma mark - Test toGeoJSONGeometry
+
     - (void) testToGeoJSONGeometry {
         XCTAssertEqualObjects([[[GFLineString alloc] initWithGeoJSONGeometry: geoJSON1] toGeoJSONGeometry], geoJSON1);
     }
 
-    - (void) testDescription {
+#pragma mark - Test description
+
+    - (void) testDescription_WithGeoJSON1 {
         XCTAssertEqualObjects([[[GFLineString alloc] initWithGeoJSONGeometry: geoJSON1] description], @"LINESTRING(100 0,101 1)");
+    }
+
+    - (void) testDescription_WithGeoJSON2 {
         XCTAssertEqualObjects([[[GFLineString alloc] initWithGeoJSONGeometry: geoJSON2] description], @"LINESTRING(102 0,102 1)");
     }
+
+#pragma mark - Test mapOverlays
 
     - (void) testMapOverlays {
     
@@ -77,51 +99,92 @@ static __attribute__((constructor(101),used,visibility("internal"))) void static
         XCTAssertTrue   ([polyline pointCount] == 2);
     }
 
-#pragma mark - Querying Tests
+#pragma mark - Test count
 
-    - (void) testCount {
-
+    - (void) testCount_WithEmptyLineString {
         XCTAssertEqual([[[GFLineString alloc] initWithWKT: @"LINESTRING()"] count], 0);
+    }
+
+    - (void) testCount_With1ElementLineString {
         XCTAssertEqual([[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60)"] count], 1);
+    }
+
+    - (void) testCount_With2ElementLineString {
         XCTAssertEqual([[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"] count], 2);
     }
 
-    - (void) testObjectAtIndex {
+#pragma mark - Test objectAtIndex
 
+    - (void) testObjectAtIndex_With2ElementLineStringAndIndex0 {
         XCTAssertEqualObjects([[[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"] pointAtIndex: 0] toWKTString], @"POINT(40 60)");
-        XCTAssertEqualObjects([[[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"] pointAtIndex: 1] toWKTString], @"POINT(120 110)");
+    }
 
+    - (void) testObjectAtIndex_With2ElementLineStringAndIndex1  {
+        XCTAssertEqualObjects([[[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"] pointAtIndex: 1] toWKTString], @"POINT(120 110)");
+    }
+
+    - (void) testObjectAtIndex_With1ElementLineStringAndOutOfRangeIndex  {
         XCTAssertThrowsSpecificNamed(([[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60)"] pointAtIndex: 1]), NSException, NSRangeException);
     }
 
-    - (void) testFirstObject {
+#pragma mark - Test firstObject
 
+    - (void) testFirstObject_With2ElementLineString {
         XCTAssertEqualObjects([[[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"] firstPoint] toWKTString], @"POINT(40 60)");
+    }
 
+    - (void) testFirstObject_WithEmptyLineString_NoThrow {
         XCTAssertNoThrow([[[GFLineString alloc] initWithWKT: @"LINESTRING()"] firstPoint]);
+    }
+
+    - (void) testFirstObject_WithEmptyLineString {
         XCTAssertEqualObjects([[[GFLineString alloc] initWithWKT: @"LINESTRING()"] firstPoint], nil);
     }
 
-    - (void) testLastObject {
+#pragma mark - Test lastObject
 
+    - (void) testLastObject_With2ElementLineString  {
         XCTAssertEqualObjects([[[[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"] lastPoint] toWKTString], @"POINT(120 110)");
+    }
 
+    - (void) testLastObject_WithEmptyLineString_NoThrow  {
         XCTAssertNoThrow([[[GFLineString alloc] initWithWKT: @"LINESTRING()"] lastPoint]);
+    }
+
+    - (void) testLastObject_WithEmptyLineString  {
         XCTAssertEqualObjects([[[GFLineString alloc] initWithWKT: @"LINESTRING()"] lastPoint], nil);
     }
 
-#pragma mark - Indexed Subscripting Tests
+#pragma mark - Test objectAtIndexSubscript
 
-    - (void) testObjectAtIndexedSubscript {
-
+    - (void) testObjectAtIndexedSubscript_With2ElementLineStringAndIndex0_NoThrow {
         GFLineString * lineString = [[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"];
 
         XCTAssertNoThrow(lineString[0]);
+    }
+
+    - (void) testObjectAtIndexedSubscript_With2ElementLineStringAndIndex1_NoThrow {
+        GFLineString * lineString = [[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"];
+
         XCTAssertNoThrow(lineString[1]);
-        XCTAssertThrowsSpecificNamed(lineString[2], NSException, NSRangeException);
+    }
+
+    - (void) testObjectAtIndexedSubscript_With2ElementLineStringAndIndex0 {
+        GFLineString * lineString = [[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"];
 
         XCTAssertEqualObjects([lineString[0] toWKTString], @"POINT(40 60)");
+    }
+
+    - (void) testObjectAtIndexedSubscript_With2ElementLineStringAndIndex1 {
+        GFLineString * lineString = [[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"];
+
         XCTAssertEqualObjects([lineString[1] toWKTString], @"POINT(120 110)");
+    }
+
+    - (void) testObjectAtIndexedSubscript_With2ElementLineStringAndOutOfRangeIndex {
+        GFLineString * lineString = [[GFLineString alloc] initWithWKT: @"LINESTRING(40 60,120 110)"];
+
+        XCTAssertThrowsSpecificNamed(lineString[2], NSException, NSRangeException);
     }
 
 @end
