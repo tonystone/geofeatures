f8002e2fb81fbc79c5a209d47ebdfadf91344aae
Tony Stone <hatter24@gmail.com>
2015-11-11 06:46:57 -0800

    Ran "pod update" to update the project for the example using frameworks.

--- Example/Pods/Headers/Public/GeoFeatures/GeoFeatures.h
+++
@@ -1,1 +0,0 @@
-../../../../../GeoFeatures/GeoFeatures.h
---
+++ Example/Pods/Target Support Files/Pods-GeoFeatures/Pods-GeoFeatures-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GeoFeaturesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GeoFeaturesVersionString[];
+
