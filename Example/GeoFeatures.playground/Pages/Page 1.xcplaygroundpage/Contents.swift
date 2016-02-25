6b92f3dbb385ad47b9f44a3d4c42cbb9ee25656c
Tony Stone <hatter24@gmail.com>
2016-02-24 19:25:28 -0800

    Corrected Page 1 of Playground for new structure.

--- Example/GeoFeatures.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Example/GeoFeatures.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -12,9 +12,9 @@ let floatingPrecision2 = FloatingPrecision()
 let fixedEqual1 = fixedPrecision1 == fixedPrecision2
 let fixedEqual2 = fixedPrecision1 == fixedPrecision3
 
-let flaotingEqual1 = floatingPrecision1 == floatingPrecision2
-let fixedFloatingEqual1 = fixedPrecision1 == floatingPrecision1
-let fixedFloatingEqual2 = fixedPrecision4 == floatingPrecision1
+floatingPrecision1 == floatingPrecision2
+fixedPrecision1 == floatingPrecision1
+fixedPrecision4 == floatingPrecision1
 
 //: ## Usage scenarios
 // Simple Array of tubles used as coordinates
@@ -22,23 +22,28 @@ let coordinates2d: [(Double, Double)]         = [(0,0), (0,7), (4,2), (2,0), (0,
 let coordinates3d: [(Double, Double, Double)] = [(0,0,0), (0,7,0), (4,2,0), (2,0,0), (0,0,0)]
 
 // LinearRing created with simple tuble array
-let ring2d = LinearRing(coordinates: coordinates2d)
+LinearRing<Coordinate2D>(coordinates: coordinates2d)
 
-let ring3d = LinearRing(coordinates:coordinates3d)
+LinearRing<Coordinate3D>(coordinates:coordinates3d)
 
-// LineString
-let lineString1 = LineString(coordinates:[(0,0,0), (0,7,0), (4,2,0), (2,0,0), (0,0,0)])
+let geometry1: Geometry = LineString<Coordinate3D>(coordinates:[(0,0,0), (0,7,0), (4,2,0), (2,0,0), (0,0,0)])
+
+if let linearType = geometry1 as? Curve {
+    linearType.length()
+} else {
+    print("Can't convert")
+}
 
 // Create a Polygon with a simple array of tuples and an array of innerRings
-let polygon1 = Polygon(outerRing: [(0,0), (0,7), (4,2), (2,0), (0,0)], innerRings: [])
+//let polygon1 = Polygon<Coordinate2D>(outerRing: [(0,0), (0,7), (4,2), (2,0), (0,0)], innerRings: [])
 
 // Create a Polygon with a tuple simaler to WKT with the syntax ([tuples], [[tuples]])
-let polygon2 = Polygon(rings: ([(0,0), (0,7), (4,2), (2,0), (0,0)],[]))
-let polygon3 = Polygon(rings: ([(0,0), (0,7), (4,2), (2,0), (0,0)],[[(0.5,0.5), (0.5,6.5), (3.5,1.5), (1.5,0.5), (0.5,0.5)]]))
+let polygon2 = Polygon<Coordinate2D>(rings: ([(0,0), (0,7), (4,2), (2,0), (0,0)],[]))
+let polygon3 = Polygon<Coordinate2D>(rings: ([(0,0), (0,7), (4,2), (2,0), (0,0)],[[(0.5,0.5), (0.5,6.5), (3.5,1.5), (1.5,0.5), (0.5,0.5)]]))
 
 // Geoemetry arrays can be constructed and used to create rthe collection types
-var geometryArray: [Geometry] = [Point(coordinate: (1,1)), Polygon()]
-var pointArray:    [Geometry] = [Point(coordinate: (1,1)), Point(coordinate: (2,2))]
+var geometryArray: [Geometry] = [Point<Coordinate2D>(coordinate: (1,1)), Polygon<Coordinate2D>()]
+var pointArray:    [Geometry] = [Point<Coordinate2D>(coordinate: (1,1)), Point<Coordinate2D>(coordinate: (2,2))]
 
 var geometryCollection1 = GeometryCollection(elements: geometryArray)
 var geometryCollection2 = GeometryCollection(elements: pointArray)
@@ -50,29 +55,23 @@ for geometry in geometryCollection1 {
     print(geometry)
 }
 
-let pointIsEmpty               = Point(coordinate: (1,1)).isEmpty()
-let polygon1IsEmpty            = Polygon().isEmpty()
-let polygon2IsEmpty            = Polygon(rings: ([(0,0), (0,7), (4,2), (2,0), (0,0)],[])).isEmpty()
-let lineString1IsEmpty         = LineString().isEmpty()
-let lineString2IsEmpty         = LineString(coordinates:[(0,0,0), (0,7,0), (4,2,0), (2,0,0), (0,0,0)]).isEmpty()
+Point<Coordinate2D>(coordinate: (1,1)).isEmpty()
+Polygon<Coordinate3DM>().isEmpty()
+Polygon<Coordinate2D>(rings: ([(0,0), (0,7), (4,2), (2,0), (0,0)],[])).isEmpty()
+LineString<Coordinate2D>().isEmpty()
+LineString<Coordinate3D>(coordinates:[(0,0,0), (0,7,0), (4,2,0), (2,0,0), (0,0,0)]).isEmpty()
 
-let geometryCollection1IsEmpty = GeometryCollection().isEmpty()
-let geometryCollection2IsEmpty = GeometryCollection(elements: [Point(coordinate: (1,1))] as [Geometry]).isEmpty()
+GeometryCollection().isEmpty()
+GeometryCollection(elements: [Point<Coordinate2D>(coordinate: (1,1))] as [Geometry]).isEmpty()
 
 // Comparison of points
-let pointsMatch1 = Point(coordinate: (1.4, 2.3)) == Point(coordinate: (1.4, 2.3))
+let pointsMatch1 = Point<Coordinate2D>(coordinate: (1.4, 2.3)) == Point<Coordinate2D>(coordinate: (1.4, 2.3))
 
-let pointsMatch2 = Point(coordinate: (1, 1)) == Point(coordinate: (1.4, 2.3))
+let pointsMatch2 = Point<Coordinate2D>(coordinate: (1, 1)) == Point<Coordinate2D>(coordinate: (1.4, 2.3))
 
 //: Algorythms
-let unionResult1 = Polygon().union(Polygon())
-let unionResult2 = Point(coordinate: (1, 1)).union(Point(coordinate: (1, 1)))
-
-if let linearType = LineString() as? protocol<Geometry, Curve> {
-    print(linearType.dimension)
-} else {
-    print("Can't convert")
-}
+Polygon<Coordinate2D>().union(Polygon<Coordinate2D>())
+Point<Coordinate2D>(coordinate: (1, 1)).union(Point<Coordinate2D>(coordinate: (1, 1)))
 
 /**
 Readers and Writers
