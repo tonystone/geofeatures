c4d230b7debdbf5c7839856b9d808c3fa718ec19
Tony Stone <hatter24@gmail.com>
2016-02-28 08:46:15 -0800

    Re-enable tuple type constructor for Polygon.

--- Geometry/Polygon.swift
+++ Geometry/Polygon.swift
@@ -40,28 +40,29 @@ public struct Polygon<CoordinateType : protocol<Coordinate, TupleConvertable>> :
         self.precision = precision
     }
     
-//    public  init<C : CollectionType where C.Generator.Element == CoordinateType.TupleType, C.Index.Distance == Int>(rings: (C,[C]), precision: Precision = defaultPrecision) {
-//        self.precision = defaultPrecision
-//        
-//        var outerRingsGenerator = rings.0.generate()
-//        
-//        self._outerRing.reserveCapacity(outerRing.count)
-//        
-//        while let coordinate = outerRingsGenerator.next() {
-//            var convertedCoordinate = CoordinateType(tuple: coordinate)
-//            
-//            self.precision.convert(&convertedCoordinate)
-//            
-//            self._outerRing.append(convertedCoordinate)
-//        }
-//        self._innerRings.reserveCapacity(innerRings.count)
-//        
-//        var innerRingsGenerator = rings.1.generate()
-//        
-//        while let ring = innerRingsGenerator.next() {
-//            self._innerRings.append(RingType(coordinates: ring, precision: precision))
-//        }
-//    }
+    public  init<C : CollectionType where C.Generator.Element == CoordinateType.TupleType, C.Index.Distance == Int>(rings: (C,[C]), coordinateReferenceSystem: CoordinateReferenceSystem = defaultCoordinateReferenceSystem, precision: Precision = defaultPrecision) {
+        
+       self.init(coordinateReferenceSystem: coordinateReferenceSystem, precision: precision)
+        
+        var outerRingsGenerator = rings.0.generate()
+        
+        self._outerRing.reserveCapacity(outerRing.count)
+        
+        while let coordinate = outerRingsGenerator.next() {
+            var convertedCoordinate = CoordinateType(tuple: coordinate)
+            
+            self.precision.convert(&convertedCoordinate)
+            
+            self._outerRing.append(convertedCoordinate)
+        }
+        self._innerRings.reserveCapacity(innerRings.count)
+        
+        var innerRingsGenerator = rings.1.generate()
+        
+        while let ring = innerRingsGenerator.next() {
+            self._innerRings.append(RingType(elements: ring, precision: precision))
+        }
+    }
     
     public  init<C : CollectionType where C.Generator.Element == CoordinateType, C.Index.Distance == Int>(outerRing: C, innerRings: [C], coordinateReferenceSystem: CoordinateReferenceSystem = defaultCoordinateReferenceSystem, precision: Precision = defaultPrecision) {
        
