acb76a53142a51b838b1e193e8aff95c14c03bec
Tony Stone <hatter24@gmail.com>
2016-02-28 19:06:18 -0800

    Implement  Shift-Add-XOR hash algorithm for combineHashes.

--- Geometry/Hash.swift
+++ Geometry/Hash.swift
@@ -6,4 +6,34 @@
 //
 //
 
-import Foundation
+import Swift
+
+/**
+    Combine mutlipile hashes producing a new hash representing the combined hash values.
+*/
+@warn_unused_result
+internal  func combineHashes(h1: Int, _ h2: Int) -> Int {
+    // Shift-Add-XOR hash algorithm
+    var hash = h1
+    
+    hash ^= (hash << 5) &+ (hash >> 2) &+ h2
+    
+    return hash
+}
+
+/**
+    Combine mutlipile hashes producing a new hash representing the combined hash values.
+*/
+@warn_unused_result
+internal  func combineHashes(h1: Int, _ h2: Int, _ h3: Int, _ rest: Int...) -> Int {
+    // Shift-Add-XOR hash algorithm
+    var hash = h1
+    
+    hash ^= (hash << 5) &+ (hash >> 2) &+ h2
+    hash ^= (hash << 5) &+ (hash >> 2) &+ h3
+    
+    for hashValue in rest {
+        hash ^= (hash << 5) &+ (hash >> 2) &+ hashValue
+    }
+    return hash
+}
