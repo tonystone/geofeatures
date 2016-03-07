9b50925d448b76351959d87ac7f5ee5909f06ca9
Paul Chang <paul@phc.io>
2016-03-06 16:09:24 -0800

    updating file header and fixing typo

--- Example/Tests/WKTReaderTests.swift
+++ Example/Tests/WKTReaderTests.swift
@@ -1,16 +1,27 @@
-//
-//  WKTReaderTests.swift
-//  GeoFeatures
-//
-//  Created by Tony Stone on 2/11/16.
-//  Copyright © 2016 CocoaPods. All rights reserved.
-//
+/*
+*   WKTReaderTests
+*
+*   Copyright 2016 Tony Stone
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*   Created by Tony Stone on 2/10/16.
+*/
 
 import XCTest
 import GeoFeatures2
 
 class WKTReaderTests: XCTestCase {
-
 
     func testRead_Point_Valid() {
         
@@ -67,7 +78,7 @@ class WKTReaderTests: XCTestCase {
         }
     }
     
-    func testRead_LineStrint_Valid() {
+    func testRead_LineString_Valid() {
         
         do {
             let geometry = try WKTReader<Coordinate2D>.read("LINESTRING(1.0 1.0, 2.0 2.0, 3.0 3.0)")
