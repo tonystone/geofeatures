9f4c3168cc5f6bf66988c6835b6aae4ae0233cfa
Tony Stone <hatter24@gmail.com>
2016-04-07 18:03:31 -0700

    Update GFRing - Removing swift_name compiler directive because this is an overloaded method of another swift_name'd method.  It was causing warnings.

--- GeoFeatures/GFRing.h
+++ GeoFeatures/GFRing.h
@@ -86,7 +86,7 @@ NS_ASSUME_NONNULL_BEGIN
      * }
      * @endcode
      */
-    - (instancetype) initWithWKT:(NSString *)wkt error: (NSError * __autoreleasing * _Nullable) error __attribute__((swift_name("init(wkt:)"), swift_error(nonnull_error)));
+    - (instancetype) initWithWKT:(NSString *)wkt error: (NSError * __autoreleasing * _Nullable) error __attribute__((swift_error(nonnull_error)));
 
     /**
      * Initialize this geometry with the given jsonDictionary.
@@ -162,7 +162,7 @@ NS_ASSUME_NONNULL_BEGIN
      * @endcode
      * @endparblock
      */
-    - (instancetype) initWithGeoJSONGeometry:(NSDictionary *)jsonDictionary  error: (NSError * __autoreleasing *) error __attribute__((swift_name("init(geoJSONGeometry:)"), swift_error(nonnull_error)));
+    - (instancetype) initWithGeoJSONGeometry:(NSDictionary *)jsonDictionary  error: (NSError * __autoreleasing *) error __attribute__((swift_error(nonnull_error)));
 @end
 
 /**
