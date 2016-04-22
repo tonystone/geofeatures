110e1e1c2785109e9b5e208ebb9d013d7078093d
Tony Stone <hatter24@gmail.com>
2016-04-22 16:58:23 -0700

    Updating docs for 1.6.0 release.

--- Docs/Doxy/html/search/functions_1.js
+++ Docs/Doxy/html/search/functions_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['boundingbox',['boundingBox',['../interface_g_f_geometry.html#a808ba7daf6d4c3d742668ad3fe5d0f0e',1,'GFGeometry']]],
-  ['boundingbox_3a',['boundingBox:',['../interface_g_f_geometry.html#a845ecba0fb20deca5dcaf3350329bf94',1,'GFGeometry']]]
+  ['boundingbox',['boundingBox',['../interface_g_f_geometry.html#a808ba7daf6d4c3d742668ad3fe5d0f0e',1,'GFGeometry']]]
 ];
--- Docs/Doxy/html/search/functions_3.js
+++ Docs/Doxy/html/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['difference_3a',['difference:',['../interface_g_f_geometry.html#acb76057cb6cdee9255ecba8447045cc6',1,'GFGeometry']]],
-  ['difference_3aerror_3a',['difference:error:',['../interface_g_f_geometry.html#ad84c62d6fee6b0169128bae069e84f53',1,'GFGeometry']]]
+  ['difference_3aerror_3a',['difference:error:',['../interface_g_f_geometry.html#a30eb0242f8e9f9e3e1d542af03e4144b',1,'GFGeometry']]]
 ];
