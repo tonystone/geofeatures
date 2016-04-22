b137f9be60d7cf809a6be82b0abf32c1c937d40f
Tony Stone <hatter24@gmail.com>
2016-04-22 16:59:17 -0700

    Updating docs for release 1.6.0.

--- search/all_0.js
+++ search/all_0.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['addgeometry_3a',['addGeometry:',['../interface_g_f_mutable_geometry_collection.html#addf8aa56e2b97984376a93a6f2e2c840',1,'GFMutableGeometryCollection::addGeometry:()'],['../interface_g_f_mutable_multi_line_string.html#adb3759e4cbd6c48eaa32046508753260',1,'GFMutableMultiLineString::addGeometry:()'],['../interface_g_f_mutable_multi_point.html#ac1a8493ed42f8d8b3962841310403f11',1,'GFMutableMultiPoint::addGeometry:()'],['../interface_g_f_mutable_multi_polygon.html#a72cf980a6b07dfa272bf6e594fe02a9d',1,'GFMutableMultiPolygon::addGeometry:()']]],
   ['addpoint_3a',['addPoint:',['../interface_g_f_mutable_line_string.html#a63a4a78093845c17233c1b3cce6465bd',1,'GFMutableLineString::addPoint:()'],['../interface_g_f_mutable_ring.html#abd2affff7a9a5efa89ee892de6c34937',1,'GFMutableRing::addPoint:()']]],
-  ['area',['area',['../interface_g_f_geometry.html#a0028dc3b2c2315a24d803dfa4cf2250e',1,'GFGeometry']]],
-  ['area_3a',['area:',['../interface_g_f_geometry.html#ace87d1a855e679bc7dad8efe01de05d9',1,'GFGeometry']]]
+  ['area',['area',['../interface_g_f_geometry.html#a0028dc3b2c2315a24d803dfa4cf2250e',1,'GFGeometry']]]
 ];
