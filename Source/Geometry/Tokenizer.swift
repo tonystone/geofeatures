0aa6f43a303356dd7dd4196ae27e86cddc838df1
Tony Stone <hatter24@gmail.com>
2016-10-19 08:23:44 -0700

    Refactor Tokenizer eliminating NSRange preferring Swift Range<> instead.

--- Source/Geometry/Tokenizer.swift
+++ Source/Geometry/Tokenizer.swift
@@ -21,7 +21,7 @@ import Swift
 import Foundation
 
 internal protocol Token {
-    func match(_ string: String, matchRange: NSRange) -> NSRange
+    func match(_ string: String, matchRange: Range<String.Index>) -> Range<String.Index>?
     func isNewLine() -> Bool
 }
 
@@ -29,20 +29,13 @@ internal class Tokenizer<T : Token> {
     
     var line = 0
     var column = 0
-
-    // FIXME: This is temporary until the the following known issue is fixed. https://github.com/apple/swift-corelibs-foundation/blob/master/Docs/Issues.md#known-issues
-#if os(Linux) || os(FreeBSD)
-    var matchString: String { get { return stringStream.bridge().substring(with: matchRange) } }
-#else
-    var matchString: String { get { return (stringStream as NSString).substring(with: matchRange) } }
-#endif
     
     fileprivate var stringStream: String
-    fileprivate var matchRange: NSRange
+    fileprivate var matchRange: Range<String.Index>
     
     init(string: String) {
         self.stringStream = string
-        self.matchRange = NSMakeRange(0, string.characters.count)
+        self.matchRange = string.startIndex..<string.endIndex
         
         if self.stringStream.characters.count > 0 {
             line = 1
@@ -51,31 +44,29 @@ internal class Tokenizer<T : Token> {
     }
     
     func accept(_ token: T) -> String? {
-        let range = token.match(stringStream, matchRange: matchRange)
-            
-        if range.location != NSNotFound {
+        if let range = token.match(stringStream, matchRange: matchRange) {
             
             if token.isNewLine() {
                 line += 1
                 column = 1
             } else {
-                column += range.length
+                column += stringStream.distance(from: range.lowerBound, to: range.upperBound)
             }
             // Increment the range for matching
-            matchRange.location += range.length
-            matchRange.length   -= range.length
+            matchRange = range.upperBound..<matchRange.upperBound
             
-    // FIXME: This is temporary until the the following known issue is fixed. https://github.com/apple/swift-corelibs-foundation/blob/master/Docs/Issues.md#known-issues
-#if os(Linux) || os(FreeBSD)
-            return stringStream.bridge().substring(with: range)
-#else
-            return (stringStream as NSString).substring(with: range)
-#endif
+            return stringStream.substring(with: range)
         }
         return nil
     }
     
     func expect(_ token: T) -> Bool {
-        return token.match(stringStream, matchRange: matchRange).location != NSNotFound
+        return token.match(stringStream, matchRange: matchRange) != nil
+    }
+    
+    var matchString: String {
+        get {
+            return stringStream.substring(with: matchRange)
+        }
     }
 }
