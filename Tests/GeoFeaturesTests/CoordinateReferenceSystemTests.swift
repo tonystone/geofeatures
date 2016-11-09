55e8033d2a23d89aca9052a8f2fd198a5ea9314f
Tony Stone <hatter24@gmail.com>
2016-11-09 14:57:17 -0800

    Add CoordinateReferenceSystemTests.swift.

---
+++ Tests/GeoFeaturesTests/CoordinateReferenceSystemTests.swift
@@ -0,0 +1,25 @@
+//
+//  CoordinateReferenceSystemTests.swift
+//  GeoFeatures
+//
+//  Created by Tony Stone on 11/9/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import GeoFeatures
+
+fileprivate struct DummyCoordinateReferenceSystem: CoordinateReferenceSystem, Equatable, Hashable {
+    public var hashValue: Int { get { return String(reflecting: self).hashValue } }
+}
+
+class CoordinateReferenceSystemTests: XCTestCase {
+
+    func testEqual_True() {
+        XCTAssertTrue(Cartesian() == Cartesian())
+    }
+
+    func testEqual_False() {
+        XCTAssertFalse(Cartesian() == DummyCoordinateReferenceSystem())
+    }
+}
