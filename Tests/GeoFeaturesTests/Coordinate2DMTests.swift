65bb7bce100218f2b39674a5db58b1fb3d76e991
Tony Stone <hatter24@gmail.com>
2016-11-17 16:40:59 -0800

    Update all CoordinateType tests to test init(array:) bounds and throw if out.

--- Tests/GeoFeaturesTests/Coordinate2DMTests.swift
+++ Tests/GeoFeaturesTests/Coordinate2DMTests.swift
@@ -63,16 +63,42 @@ class Coordinate2DMTests: XCTestCase {
 
     // MARK: _ArrayConstructable
 
-    func testInit_Array () {
-        let coordinate = Coordinate2DM(array: [2.0, 3.0, 4.0])
+    func testInit_Array () throws {
+        let coordinate = try Coordinate2DM(array: [2.0, 3.0, 4.0])
 
         XCTAssertEqual(coordinate.x, 2.0)
         XCTAssertEqual(coordinate.y, 3.0)
         XCTAssertEqual(coordinate.m, 4.0)
     }
 
-    func testInit_Array_Invalid () {
-        // TODO: Can't test precondition at this point due to lack of official support in Swift.
+    func testInit_Array_Invalid_ToSmall() {
+
+        let input = [2.0, 3.0]
+        let expected = "Invalid array size (2)."
+
+        XCTAssertThrowsError(try Coordinate2DM(array: input)) { error in
+
+            if case _ArrayConstructableError.invalidArraySize(let message) = error {
+                XCTAssertEqual(message, expected)
+            } else {
+                XCTFail("Wrong error thrown: \(error) is not equal to \(expected)")
+            }
+        }
+    }
+
+    func testInit_Array_Invalid_ToLarge() {
+
+        let input = [2.0, 3.0, 4.0, 5.0]
+        let expected = "Invalid array size (4)."
+
+        XCTAssertThrowsError(try Coordinate2DM(array: input)) { error in
+
+            if case _ArrayConstructableError.invalidArraySize(let message) = error {
+                XCTAssertEqual(message, expected)
+            } else {
+                XCTFail("Wrong error thrown: \(error) is not equal to \(expected)")
+            }
+        }
     }
 
     // MARK: CopyConstructable
