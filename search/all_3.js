6d8a99c97a0138f4a4928e48c9afe28916b538e5
Tony Stone <hatter24@gmail.com>
2017-09-04 16:55:33 -0700

    Release 1.7.0 documentation update.

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['deprecated_20list',['Deprecated List',['../deprecated.html',1,'']]],
   ['difference_3a',['difference:',['../interface_g_f_geometry.html#acb76057cb6cdee9255ecba8447045cc6',1,'GFGeometry']]],
   ['difference_3aerror_3a',['difference:error:',['../interface_g_f_geometry.html#a30eb0242f8e9f9e3e1d542af03e4144b',1,'GFGeometry']]]
 ];
