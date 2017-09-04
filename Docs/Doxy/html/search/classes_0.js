05d84268c2cdc249ac471e4521ddc122a0a54858
Tony Stone <hatter24@gmail.com>
2017-09-04 16:44:57 -0700

    Updating doxygen docs.

--- Docs/Doxy/html/search/classes_0.js
+++ Docs/Doxy/html/search/classes_0.js
@@ -7,7 +7,6 @@ var searchData=
   ['gfgeometry_28wkt_29',['GFGeometry(WKT)',['../category_g_f_geometry_07_w_k_t_08.html',1,'']]],
   ['gfgeometrycollection',['GFGeometryCollection',['../interface_g_f_geometry_collection.html',1,'']]],
   ['gflinestring',['GFLineString',['../interface_g_f_line_string.html',1,'']]],
-  ['gflinestringabstract',['GFLineStringAbstract',['../interface_g_f_line_string_abstract.html',1,'']]],
   ['gfmultilinestring',['GFMultiLineString',['../interface_g_f_multi_line_string.html',1,'']]],
   ['gfmultipoint',['GFMultiPoint',['../interface_g_f_multi_point.html',1,'']]],
   ['gfmultipolygon',['GFMultiPolygon',['../interface_g_f_multi_polygon.html',1,'']]],
@@ -21,8 +20,6 @@ var searchData=
   ['gfmutablepolygon',['GFMutablePolygon',['../interface_g_f_mutable_polygon.html',1,'']]],
   ['gfmutablering',['GFMutableRing',['../interface_g_f_mutable_ring.html',1,'']]],
   ['gfpoint',['GFPoint',['../interface_g_f_point.html',1,'']]],
-  ['gfpointabstract',['GFPointAbstract',['../interface_g_f_point_abstract.html',1,'']]],
   ['gfpolygon',['GFPolygon',['../interface_g_f_polygon.html',1,'']]],
-  ['gfpolygonabstract',['GFPolygonAbstract',['../interface_g_f_polygon_abstract.html',1,'']]],
   ['gfring',['GFRing',['../interface_g_f_ring.html',1,'']]]
 ];
--- Docs/Doxy/html/search/searchdata.js
+++ Docs/Doxy/html/search/searchdata.js
@@ -2,23 +2,20 @@ var indexSectionsWithContent =
 {
   0: "abcdfgilmoprstuwxy",
   1: "g",
-  2: "abcdfgilmoprstuwxy",
-  3: "d"
+  2: "abcdfgilmoprstuwxy"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions",
-  3: "pages"
+  2: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions",
-  3: "Pages"
+  2: "Functions"
 };
 
